00b6cb82e20cd69fc1395c2a51c5b02150ec0439
Douglas <douglasmodolo@outlook.com.br>
2025-03-16 18:15:13 -0300

    Criação de transação

--- Fundamentos Técnicos e Estratégicos/Nivelamento/Criando API REST com Node.js/src/routes/transactions.ts
+++ Fundamentos Técnicos e Estratégicos/Nivelamento/Criando API REST com Node.js/src/routes/transactions.ts
@@ -1,20 +1,28 @@
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
 import { knex } from '../database'
 
 
 export async function transactionsRoutes(app: FastifyInstance) {
     
-    app.get('/hello', async () => {
-        // const transaction = await knex('transactions').insert({
-        //     id: crypto.randomUUID(),
-        //     title: 'Transações de teste',
-        //     amount: 1000,
-        // }).returning('*')
+    app.post('/', async (request, reply) => {    
+        // { title, amount, type: credit/debit }
+        
+        const createTransactionBodySchema = z.object({
+            title: z.string().nonempty(),
+            amount: z.number(),
+            type: z.enum(['credit','debit'])
+        })
+
+        const body = createTransactionBodySchema.parse(request.body)
+
+        await knex('transactions')
+            .insert({
+                id: crypto.randomUUID(),
+                title: body.title,
+                amount: body.type == 'credit' ? body.amount : body.amount * -1,
+            })
     
-        const transaction = await knex('transactions')
-            .where('amount', 1000)
-            .select('*') 
-    
-        return transaction
+        return reply.status(201).send()
     })
 }
